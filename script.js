53f90a1edea3865b54fa88360a03f02a1491d53e
Sebastian Nowicki <108746110+sebasnow777@users.noreply.github.com>
2024-04-28 00:32:14 +0900

    HTML/CSS complete
    Javascript started adding functions

---
+++ script.js
@@ -0,0 +1,29 @@
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+console.log(board);
+
+//Define game variables
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+// Draw Snake
+
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+    });
+
+}
+
+//Create a snake or food cube/div
+
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+}
